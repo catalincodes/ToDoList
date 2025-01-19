728f9633c85cd228983b145947ff8a7bcf1e5941
Catalin Codes <12548925+catalincodes@users.noreply.github.com>
2025-01-19 11:34:59 -0500

    Initial Boilerplate for ToDoListDisplayView
    - Add ToDoListDisplayView
    - Add the view to the MainWindow

---
+++ Views/ToDoListDisplayView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ToDoList.Views
+{
+	/// <summary>
+	/// Interaction logic for ToDoListDisplayView.xaml
+	/// </summary>
+	public partial class ToDoListDisplayView : UserControl
+	{
+		public ToDoListDisplayView()
+		{
+			InitializeComponent();
+		}
+	}
+}
