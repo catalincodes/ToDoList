e8dbf52d014ff8eed0bbaafefca6df6ccd155a10
Catalin Codes <12548925+catalincodes@users.noreply.github.com>
2025-01-19 12:39:20 -0500

    Added AddTask View - Added the view - Added and styled the header, textbox and button - Displayed it for now in the MainWindow

---
+++ Views/AddTaskView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ToDoList.Views
+{
+	/// <summary>
+	/// Interaction logic for AddTask.xaml
+	/// </summary>
+	public partial class AddTaskView : UserControl
+	{
+		public AddTaskView()
+		{
+			InitializeComponent();
+		}
+	}
+}
